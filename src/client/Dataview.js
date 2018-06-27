84b9b3da0fc7155b30f4c8fb93d9c6b1595390b7
David Eliason <david@davethemaker.com>
2018-06-27 12:08:33 -0700

    update Dataview

---
+++ src/client/Dataview.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class Dataview extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    return (
+          <div>
+              <p>
+hello
+</p>
+            </div>
+    );
+  }
+}
+
+export default Dataview;
